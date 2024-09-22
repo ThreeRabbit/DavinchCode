ed469d0bce6c93030359156e8bb657a75b358443
Jo_Chanhee <threerabbitgames@gmail.com>
2024-09-23 04:07:40 +0900

    GPGS 제거, TRP 엽데이트

--- Assets/02.Scripts/Manager/GPGSManager.cs
+++ Assets/02.Scripts/Manager/GPGSManager.cs
@@ -2,8 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using ThreeRabbitPackage.DesignPattern;
-using GooglePlayGames;
-using GooglePlayGames.BasicApi;
+//using GooglePlayGames;
+//using GooglePlayGames.BasicApi;
 using UnityEngine.Events;
 using System.Threading.Tasks;
 
@@ -16,71 +16,73 @@ public class GPGSManager : TRSingleton<GPGSManager>
 
 	public void Init(UnityAction success = null, UnityAction fail = null)
 	{
-		// GPGS 플러그인 설정
-		PlayGamesClientConfiguration config = new PlayGamesClientConfiguration
-			.Builder()
-			.RequestServerAuthCode(false)
-			.RequestEmail()
-			.RequestIdToken()
-			.Build();
+        //// GPGS 플러그인 설정
+        //PlayGamesClientConfiguration config = new PlayGamesClientConfiguration
+        //    .Builder()
+        //    .RequestServerAuthCode(false)
+        //    .RequestEmail()
+        //    .RequestIdToken()
+        //    .Build();
 
-		// 커스텀 된 정보로 GPGS 초기화
-		PlayGamesPlatform.InitializeInstance(config);
-		PlayGamesPlatform.DebugLogEnabled = true;
+        //// 커스텀 된 정보로 GPGS 초기화
+        //PlayGamesPlatform.InitializeInstance(config);
+        //PlayGamesPlatform.DebugLogEnabled = true;
 
-		// GPGS 활성화
-		if(PlayGamesPlatform.Activate() != null)
-		{
-			success?.Invoke();
-		}
-		else
-		{
-			fail?.Invoke();
-		}
-	}
+        //// GPGS 활성화
+        //if (PlayGamesPlatform.Activate() != null)
+        //{
+        //    success?.Invoke();
+        //}
+        //else
+        //{
+        //    fail?.Invoke();
+        //}
+    }
 
 	public Task<bool> GPGSLoginAsync(UnityAction success = null, UnityAction fail = null)
 	{
         var tcs = new TaskCompletionSource<bool>();
 
         // 이미 로그인 된 경우
-        if (PlayGamesPlatform.Instance.localUser.authenticated == true)
-		{
-			success?.Invoke();
-            tcs.SetResult(true);
-		}
-		else
-		{
-            PlayGamesPlatform.Instance.localUser.Authenticate((bool isSuccess, string callback) => {
-				if (isSuccess)
-				{
-                    success?.Invoke();
-                    tcs.SetResult(true);
-                }
-				else
-				{
-                    fail?.Invoke();
-					Debug.Log($"GPGSLogin Fail: {callback}");
-                    tcs.SetResult(false);
-                }
-			});
-		}
+        //if (PlayGamesPlatform.Instance.localUser.authenticated == true)
+        //{
+        //    success?.Invoke();
+        //    tcs.SetResult(true);
+        //}
+        //else
+        //{
+        //    PlayGamesPlatform.Instance.localUser.Authenticate((bool isSuccess, string callback) =>
+        //    {
+        //        if (isSuccess)
+        //        {
+        //            success?.Invoke();
+        //            tcs.SetResult(true);
+        //        }
+        //        else
+        //        {
+        //            fail?.Invoke();
+        //            Debug.Log($"GPGSLogin Fail: {callback}");
+        //            tcs.SetResult(false);
+        //        }
+        //    });
+        //}
 
         return tcs.Task;
 	}
 
 	public string GetTokens()
     {
-		if (PlayGamesPlatform.Instance.localUser.authenticated)
-		{
-			// 유저 토큰 받기 
-			string _IDtoken = PlayGamesPlatform.Instance.GetIdToken();
-			return _IDtoken;
-		}
-		else
-		{
-			Debug.Log("접속되어 있지 않습니다. PlayGamesPlatform.Instance.localUser.authenticated :  fail");
-			return null;
-		}
+        //if (PlayGamesPlatform.Instance.localUser.authenticated)
+        //{
+        //    // 유저 토큰 받기 
+        //    string _IDtoken = PlayGamesPlatform.Instance.GetIdToken();
+        //    return _IDtoken;
+        //}
+        //else
+        //{
+        //    Debug.Log("접속되어 있지 않습니다. PlayGamesPlatform.Instance.localUser.authenticated :  fail");
+        //    return null;
+        //}
+        return null;
     }
 }
